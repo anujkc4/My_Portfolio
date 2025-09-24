43c710a54405c7e2d94008a2166289df859dbb85
anujkc4 <anujkc000@gmail.com>
2025-09-24 22:40:50 +0545

    file modify

---
+++ script.js
@@ -0,0 +1,44 @@
+const cursor = document.querySelector('.custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+    cursor.style.top = `${e.clientY}px`;
+    cursor.style.left = `${e.clientX}px`;
+});
+
+// Optional: grow cursor when hovering clickable items
+document.querySelectorAll('a, button, li').forEach((el) => {
+    el.addEventListener('mouseenter', () => {
+        cursor.style.transform = 'translate(-50%, -50%) scale(1.5)';
+        cursor.style.background = 'rgba(0, 172, 238, 0.7)';
+    });
+    el.addEventListener('mouseleave', () => {
+        cursor.style.transform = 'translate(-50%, -50%) scale(1)';
+        cursor.style.background = 'rgba(255, 255, 255, 0.7)';
+    });
+});
+
+
+// +++++++++++++++++++++++++++++++++++++
+
+
+var typed = new Typed('#element', {
+  strings: [
+    'a full stack developer.',
+    'a web-developer.',
+    '&amp; web-designer.'
+  ],
+  typeSpeed: 80,
+  backSpeed: 40,
+  backDelay: 1000,
+  loop: true,
+  showCursor: true,
+});
+
+
+
+
+
+let btn = document.getElementById("btn");
+btn.addEventListener(("click"), ()=>{
+  a
+})
